e5be2fa3ea7e2b50304d570d66d8ec60520498a9
Rian T Gray <s3397762@student.rmit.edu.au>
2019-08-16 20:20:46 +1000

    Add Model and Services - Completed Section 5

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -2,23 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Models;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _MailService;
+
+        public AppController(IMailService _mailService)
+        {
+
+            _MailService = _mailService;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Contact ()
+        [HttpGet("contact")]
+        public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
             return View();
         }
 
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //send email
+                _MailService.SendMessage("enquiries@tanyablack.com", model.Subject, $"From: {model.Name} {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                //show the errors
+            }
+
+            return View();
+        }
+
+        [HttpGet("about")]
         public IActionResult About()
         {
             ViewBag.Title = "About Us";

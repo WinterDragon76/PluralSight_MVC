93db10f90d2abac79dd3ed953b590520e122b5da
Rian T Gray <s3397762@student.rmit.edu.au>
2019-08-17 18:19:05 +1000

    Initial Migration Success

--- DutchTreat/Program.cs
+++ DutchTreat/Program.cs
@@ -10,16 +10,26 @@ using Microsoft.Extensions.Logging;
 
 namespace DutchTreat
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      BuildWebHost(args).Run();
-    }
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
 
-    public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
-  }
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(SetupConfiguration)
+                .UseStartup<Startup>()
+                .Build();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            //Removing the default configuration options
+            builder.Sources.Clear();
+            //treated as a hierarchy
+            builder.AddJsonFile("config.json", false, true)
+                .AddEnvironmentVariables();
+        }
+    }
 }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,45 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
 {
-  public class Startup
-  {
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-    public void ConfigureServices(IServiceCollection services)
+    public class Startup
     {
-      services.AddTransient<IMailService, NullMailService>();
-      // Support for real mail service
+        private readonly IConfiguration _config;
 
-      services.AddMvc();
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IMailService, NullMailService>();
+            // Support for real mail service
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+
+                services.AddMvc();
+            });
+        }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseStaticFiles();
+                app.UseNodeModules(env);
+
+                app.UseMvc(cfg =>
+                {
+                    cfg.MapRoute("Default",
+                "{controller}/{action}/{id?}",
+                new { controller = "App", Action = "Index" });
+                });
+
+            }
+        }
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
-      app.UseStaticFiles();
-      app.UseNodeModules(env);
-
-      app.UseMvc(cfg =>
-      {
-        cfg.MapRoute("Default", 
-          "{controller}/{action}/{id?}", 
-          new { controller = "App", Action = "Index" });
-      });
-
-    }
-  }
-}
